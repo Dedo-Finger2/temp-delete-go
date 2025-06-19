937cdeaf7bca2efa6ef0c8e89ccfd63559a0ce94
Antonio Mauricio <mauricio.almeida.dev@gmail.com>
2025-06-19 06:30:40 -0300

    feat: adds removing temp and percent temp logic

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/user"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	const REGULAR_TEMP_PATH = "C:\\Windows\\Temp"
+
+	u, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	percentTempPath := fmt.Sprintf("%s\\AppData\\Local\\Temp", u.HomeDir)
+
+	rtEntries, err := os.ReadDir(REGULAR_TEMP_PATH)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, e := range rtEntries {
+		os.RemoveAll(REGULAR_TEMP_PATH + "\\" + e.Name())
+	}
+
+	fmt.Println("Regular temp cleaned!")
+
+	ptEntries, err := os.ReadDir(percentTempPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, e := range ptEntries {
+		os.RemoveAll(percentTempPath + "\\" + e.Name())
+	}
+
+	fmt.Println("Percent temp cleaned!")
+
+	fmt.Scanln()
 }
